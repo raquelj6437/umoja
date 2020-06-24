3a256522e9decb040a343c34189b69f917271e8d
raqueljoseph <raquelajoseph@gmail.com>
2020-06-24 03:03:20 -0400

    fix navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,17 @@
-$(document).ready(function(){
-  $(window).resize(function() {
-    if ($(this).width() < 1024) {
-      $('#umoja-logo').hide();
-      $('#home-button').show();
-    } else {
-      $('#umoja-logo').show();
-      $('#home-button').hide();
-      }
-  });
-});
+// $(document).ready(function(){
+//   setTimeout(function () {
+//     $(window).resize(function() {
+//       if ($(this).width() < 1024) {
+//         $('#umoja-logo').hide();
+//         $('#home-button').show();
+//       } else {
+//         $('#umoja-logo').show();
+//         $('#home-button').hide();
+//         }
+//     });
+//   }, 1000);
+// });
+//
+// jQuery(document).ready(function () {
+//
+// });
