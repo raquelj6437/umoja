86e94a4ac8286a8310c1797dd49ddb0a92240650
raqueljoseph <raquelajoseph@gmail.com>
2020-06-24 21:01:27 -0400

    fix error message

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,3 @@
-// $(document).ready(function(){
-//   setTimeout(function () {
-//     $(window).resize(function() {
-//       if ($(this).width() < 1024) {
-//         $('#umoja-logo').hide();
-//         $('#home-button').show();
-//       } else {
-//         $('#umoja-logo').show();
-//         $('#home-button').hide();
-//         }
-//     });
-//   }, 1000);
-// });
-//
-// jQuery(document).ready(function () {
-//
-// });
+$(document).ready(function(){
+  $(".ui-loader").hide()
+});
