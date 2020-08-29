11c6d5b95f3e232f64b9f3c23d291df9fcb060ac
raqueljoseph <raquelajoseph@gmail.com>
2020-08-29 00:15:25 -0400

    add firebase

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
-$(document).ready(function(){
-  $(".ui-loader").hide()
+$(document).ready(function() {
+  function writeUserData(name, email, message) {
+    firebase.database().ref(d.getMonth().toString() +'-'+ d.getDate().toString()+'/').push().set({
+      name: name,
+      email: email,
+      message: message
+    });
+  }
+  $("#submit").click(function() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+
+    if (name == '' || email == '' || message == ''){
+      alert("Please fill in all the fields.");
+    }
+    else {
+      writeUserData(name, email, message);
+    }
+  });
+
+  var d = new Date();
 });
