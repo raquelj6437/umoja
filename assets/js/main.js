92d36f9fdb102503517f9eafd9978cabaead0294
raqueljoseph <raquelajoseph@gmail.com>
2020-09-07 23:00:31 -0400

    add instagram

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
+  //contact firebase
   function writeUserData(name, email, message) {
-    firebase.database().ref(d.getMonth().toString() +'-'+ d.getDate().toString()+'/').push().set({
+    firebase.database().ref(d.getMonth().toString() + '-' + d.getDate().toString() + '/').push().set({
       name: name,
       email: email,
       message: message
@@ -11,10 +12,9 @@ $(document).ready(function() {
     var email = $("#email").val();
     var message = $("#message").val();
 
-    if (name == '' || email == '' || message == ''){
+    if (name == '' || email == '' || message == '') {
       alert("Please fill in all the fields.");
-    }
-    else {
+    } else {
       writeUserData(name, email, message);
       alert("Thank you for your response!")
       $("#name").val('');
@@ -26,6 +26,43 @@ $(document).ready(function() {
   var d = new Date();
 
   $('#gallery-button').click(function() {
-      alert('Coming Soon.')
+    alert('Coming Soon.')
+  });
+
+  //instgram firebase
+  function writePostIDData(snap) {
+    firebase.database().ref('id').push().set({
+      snap: snap
+    });
+  }
+  $("#IDSubmit").click(function() {
+    alert('clicked')
+    var id = $("#ID").val();
+
+    writePostIDData(id);
+  });
+
+  setTimeout(
+  function()
+  {
+    $("#feed").append('<div class="col-sm"> <iframe class="instagram-media instagram-media-rendered" id="instagram-embed-0" src="https://www.instagram.com/p/'+ postIDList[0] + '/embed/captioned/?cr=1&amp;v=10&amp;wp=1080&amp;rd=https%3A%2F%2Fblog.hubspot.com&amp;rp=%2Fmarketing%2Fembed-social-media-posts-guide#%7B%22ci%22%3A0%2C%22os%22%3A3610%7D" allowtransparency="true" allowfullscreen="true" frameborder="0" data-instgrm-payload-id="instagram-media-payload-0" scrolling="no" style="max-width: 540px; width: calc(100% - 2px); background-color: white; box-shadow: none; display: block; min-width: 326px; padding: 0px;"></iframe> </div>');
+    $("#feed").append('<div class="col-sm"> <iframe class="instagram-media instagram-media-rendered" id="instagram-embed-0" src="https://www.instagram.com/p/'+ postIDList[1] + '/embed/captioned/?cr=1&amp;v=10&amp;wp=1080&amp;rd=https%3A%2F%2Fblog.hubspot.com&amp;rp=%2Fmarketing%2Fembed-social-media-posts-guide#%7B%22ci%22%3A0%2C%22os%22%3A3610%7D" allowtransparency="true" allowfullscreen="true" frameborder="0" data-instgrm-payload-id="instagram-media-payload-0" scrolling="no" style="max-width: 540px; width: calc(100% - 2px); background-color: white; box-shadow: none; display: block; min-width: 326px; padding: 0px;"></iframe> </div>');
+    $("#feed").append('<div class="col-sm"> <iframe class="instagram-media instagram-media-rendered" id="instagram-embed-0" src="https://www.instagram.com/p/'+ postIDList[2] + '/embed/captioned/?cr=1&amp;v=10&amp;wp=1080&amp;rd=https%3A%2F%2Fblog.hubspot.com&amp;rp=%2Fmarketing%2Fembed-social-media-posts-guide#%7B%22ci%22%3A0%2C%22os%22%3A3610%7D" allowtransparency="true" allowfullscreen="true" frameborder="0" data-instgrm-payload-id="instagram-media-payload-0" scrolling="no" style="max-width: 540px; width: calc(100% - 2px); background-color: white; box-shadow: none; display: block; min-width: 326px; padding: 0px;"></iframe> </div>');
+
+    console.log(postIDList[0])
+  }, 1000);
+
+
+  postIDList = []
+
+  var ref = firebase.database().ref('id/').orderByKey();
+  ref.once("value").then(function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+
+      var childData = childSnapshot.val();
+      postIDList.unshift(childData['snap'])
+    });
   });
 });
+
+// <div class="col-sm"> <iframe class="instagram-media instagram-media-rendered" id="instagram-embed-0" src="https://www.instagram.com/p/CE2G8nTgE9a/embed/captioned/?cr=1&amp;v=10&amp;wp=1080&amp;rd=https%3A%2F%2Fblog.hubspot.com&amp;rp=%2Fmarketing%2Fembed-social-media-posts-guide#%7B%22ci%22%3A0%2C%22os%22%3A3610%7D" allowtransparency="true" allowfullscreen="true" frameborder="0" data-instgrm-payload-id="instagram-media-payload-0" scrolling="no" style="max-width: 540px; width: calc(100% - 2px); background-color: white; box-shadow: none; display: block; min-width: 326px; padding: 0px;"></iframe> </div>
