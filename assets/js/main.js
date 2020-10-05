98fa1935d8ec4405b4e40d4c9b5b3b957d872b6f
raqueljoseph <raquelajoseph@gmail.com>
2020-10-05 13:24:31 -0400

    remove caption from instagram posts

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,7 @@ $(document).ready(function() {
   {
     var i;
     for (i = 0; i < 3; i++) {
-      $("#feed").append('<div class="col-sm"> <iframe class="instagram-media instagram-media-rendered" id="instagram-embed-0" src="https://www.instagram.com/p/'+ postIDList[i] + '/embed/captioned/?cr=1&amp;v=10&amp;wp=1080&amp;rd=https%3A%2F%2Fblog.hubspot.com&amp;rp=%2Fmarketing%2Fembed-social-media-posts-guide#%7B%22ci%22%3A0%2C%22os%22%3A3610%7D" allowtransparency="true" allowfullscreen="true" frameborder="0" data-instgrm-payload-id="instagram-media-payload-0" scrolling="no" style="max-width: 540px; width: calc(100% - 2px); background-color: white; box-shadow: none; display: block; min-width: 326px; padding: 0px;"></iframe> </div>');
+      $("#feed").append('<div class="col-sm"> <iframe class="instagram-media instagram-media-rendered" id="instagram-embed-0" src="https://www.instagram.com/p/'+ postIDList[i] + '/embed/?cr=1&amp;v=10&amp;wp=1080&amp;rd=https%3A%2F%2Fblog.hubspot.com&amp;rp=%2Fmarketing%2Fembed-social-media-posts-guide#%7B%22ci%22%3A0%2C%22os%22%3A3610%7D" allowtransparency="true" allowfullscreen="true" frameborder="0" data-instgrm-payload-id="instagram-media-payload-0" scrolling="no" style="max-width: 540px; width: calc(100% - 2px); background-color: white; box-shadow: none; display: block; min-width: 326px; padding: 0px;"></iframe> </div>');
     }
     console.log(postIDList[0])
   }, 1000);
